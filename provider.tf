b74c1803ed1b4af1ed8c5a241799f45359d4bc02
Vince Bailey <baileyvw@users.noreply.github.com>
2019-08-22 18:47:59 +0100

    update to key info
    remove key

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" { 
-    access_key = "AKIAIQVOLJHBOAYDGJ7A"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "${var.AWS_REGION}"
 }
